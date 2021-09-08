c9da347d37f8ab9adb475ed4aceb57ca4631b92b
Grzegorz Ilnicki <grzegorz.ilnicki@ubot3d.pl>
2021-09-08 16:33:33 +0200

    P-1.5: Extract command line parameter group parts with CLI switches.

--- src/cpp/SlicerParams.cpp
+++ src/cpp/SlicerParams.cpp
@@ -5,15 +5,18 @@
 #include <regex>
 #include <string_view>
 
+static const std::string kParamLinePrefix = "    ";
+static const std::string kParamSwitchPrefixLong = "--";
+static const std::string kParamSwitchPrefixShort = "-";
+
 std::vector<std::pair<std::string, std::vector<std::string>>> SlicerParams::extractParameterGroups(const std::vector<std::string>& lines)
 {
 	std::vector<std::pair<std::string, std::vector<std::string>>> lineGroups;
 	std::string currentGroupName = "Basic IO config";
 	lineGroups.push_back({currentGroupName, {}});
-	const std::string firstLineUsageHintPart = "[ OPTIONS ] [ file.stl ] [ file2.stl ] ...";
-	const std::string paramLinePrefix = "    ";
-	const std::string groupNameLinePrefix = "  ";
-	const std::string groupNameLinePostfix = ":";
+	static const std::string firstLineUsageHintPart = "[ OPTIONS ] [ file.stl ] [ file2.stl ] ...";
+	static const std::string groupNameLinePrefix = "  ";
+	static const std::string groupNameLinePostfix = ":";
 	size_t currentPairIndex = 0;
 
 	for (const auto& line : lines)
@@ -28,7 +31,7 @@ std::vector<std::pair<std::string, std::vector<std::string>>> SlicerParams::extr
 			continue;
 		}
 
-		if (line.starts_with(paramLinePrefix))
+		if (line.starts_with(kParamLinePrefix))
 		{
 			lineGroups[currentPairIndex].second.push_back(line);
 			continue;
@@ -58,6 +61,64 @@ std::vector<std::pair<std::string, std::vector<std::string>>> SlicerParams::extr
 	return lineGroups;
 }
 
+std::vector<std::string> SlicerParams::extractParamSwitchParts(const std::vector<std::string>& lineGroup)
+{
+	std::vector<std::string> paramSwitchParts;
+
+	for (const auto& line : lineGroup)
+	{
+		std::string lineLeft = line.substr(kParamLinePrefix.size(), std::string::npos);
+		const bool isLongPrefixFirst = lineLeft.starts_with(kParamSwitchPrefixLong);
+		if (!isLongPrefixFirst && !lineLeft.starts_with(kParamSwitchPrefixShort))
+			continue;
+
+		// Tokenize by whitespaces.
+		static const std::string wordDelimiter(" ");
+		static const std::regex wordDelimiterRegex(wordDelimiter);
+		const std::vector<std::string> paramLineWords(std::sregex_token_iterator(lineLeft.begin(), lineLeft.end(), wordDelimiterRegex, -1),
+													  std::sregex_token_iterator());
+
+		size_t currWordIndex = 0;
+		paramSwitchParts.push_back(paramLineWords[currWordIndex]);
+		std::string& currPart = paramSwitchParts.back();
+		const std::string paramSwitchDelimiter = ",";
+		if (currPart.ends_with(paramSwitchDelimiter))
+		{// We also have the longer switch.
+			if (isLongPrefixFirst)
+			{// The longer prefix should be given as the second one).
+				std::cout << " ### " << __FUNCTION__ << " ERROR: Unexpected param prefix order, line: " << line << std::endl;
+				exit(-1);
+			}
+			currPart.append(wordDelimiter).append(paramLineWords[++currWordIndex]);
+		}
+
+		if  (1 == paramLineWords.size())
+		{// Some parameter parts are longer that ususal,
+		 // so a newline is inserted before the description.
+		 // Thus the switch part ends here and is fully initialized.
+			continue;
+		}
+
+		++currWordIndex;
+		static const std::string paramTypePrefix = "<";
+		static const std::string paramTypePostfix = ">";
+		if (paramLineWords[currWordIndex].starts_with(paramTypePrefix) &&
+			paramLineWords[currWordIndex].ends_with(paramTypePostfix))
+		{// We also have the param type.
+			currPart.append(wordDelimiter).append(paramLineWords[currWordIndex]);
+		}
+		// The switch part ends here and is fully initialized.
+	}
+
+	return paramSwitchParts;
+}
+
+json SlicerParams::constructJsonParamSchema(const std::vector<std::string>& lineTokens)
+{
+	return json();
+}
+
+
 SlicerParams::SlicerParams(std::string cliParamsPath)
 {
 	std::ifstream fileStream(cliParamsPath);
@@ -72,7 +133,7 @@ SlicerParams::SlicerParams(std::string cliParamsPath)
 	fileStream.close();
 
 	// Read in parameter lines  - tokenize by newline delimiter.
-	std::regex delimiter("\n");
+	const std::regex delimiter("\n");
 	const std::vector<std::string> lines(std::sregex_token_iterator(fileContents.begin(), fileContents.end(), delimiter, -1),
 										 std::sregex_token_iterator());
 
@@ -81,6 +142,22 @@ SlicerParams::SlicerParams(std::string cliParamsPath)
 
 	for (const auto& [groupName, lineGroup]: lineGroups)
 	{
+		const auto paramSwitchParts = extractParamSwitchParts(lineGroup);
+
+		std::cout << " ### " << __FUNCTION__ << " PARAM SWITCH PARTS IN GROUP:" << std::endl;
+		for(const auto& token : paramSwitchParts)
+		{
+			std::cout << token << std::endl;
+		}
+		std::cout << std::endl;
+
+//		for (const auto& line : lineGroup)
+//		{
+//			const auto paramLineTokens = extractParamSwitchTokens(line);
+//			const json param = constructJsonParamSchema(paramLineTokens);
+//		}
+
+//		_params[groupName] = getJsonParamSchema(getParamLineTokens())
 		std::cout << " ### " << __FUNCTION__ << " next group:" << groupName << std::endl;
 
 		for (const auto& line: lineGroup)
--- src/cpp/SlicerParams.h
+++ src/cpp/SlicerParams.h
@@ -13,7 +13,11 @@ public:
 	SlicerParams(std::string paramsPath);
 
 private:
-	std::vector<std::pair<std::string, std::vector<std::string>>> extractParameterGroups(const std::vector<std::string>& lines);
+	static std::vector<std::pair<std::string, std::vector<std::string>>> extractParameterGroups(const std::vector<std::string>& lines);
+	static std::vector<std::string> extractParamLineTokens(const std::string& line);
+	static json constructJsonParamSchema(const std::vector<std::string>& lineTokens);
+	static std::vector<std::string> extractParamSwitchParts(const std::vector<std::string>& lineGroup);
+
 
 	json _params;
 };
