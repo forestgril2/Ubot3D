9152688a16eb3faf60d48314c40e9d4ae9ad9118
Grzegorz Ilnicki <grzegorz.ilnicki@ubot3d.pl>
2021-09-08 13:00:25 +0200

    P-1.4: Divide parameter lines into groups with group names.

--- src/cpp/SlicerParams.cpp
+++ src/cpp/SlicerParams.cpp
@@ -5,6 +5,59 @@
 #include <regex>
 #include <string_view>
 
+std::vector<std::pair<std::string, std::vector<std::string>>> SlicerParams::extractParameterGroups(const std::vector<std::string>& lines)
+{
+	std::vector<std::pair<std::string, std::vector<std::string>>> lineGroups;
+	std::string currentGroupName = "Basic IO config";
+	lineGroups.push_back({currentGroupName, {}});
+	const std::string firstLineUsageHintPart = "[ OPTIONS ] [ file.stl ] [ file2.stl ] ...";
+	const std::string paramLinePrefix = "    ";
+	const std::string groupNameLinePrefix = "  ";
+	const std::string groupNameLinePostfix = ":";
+	size_t currentPairIndex = 0;
+
+	for (const auto& line : lines)
+	{
+		if (line.ends_with(firstLineUsageHintPart))
+		{// Omit the command line usage hint.
+			continue;
+		}
+
+		if (line.empty())
+		{// Omit the command line usage hint.
+			continue;
+		}
+
+		if (line.starts_with(paramLinePrefix))
+		{
+			lineGroups[currentPairIndex].second.push_back(line);
+			continue;
+		}
+
+		if (!line.starts_with(groupNameLinePrefix))
+		{//Something is wrong.
+			std::cout << " ### " << __FUNCTION__ << " ERROR: Unexpected line prefix, line: " << line << std::endl;
+			exit(-1);
+		}
+
+		if (!line.ends_with(groupNameLinePostfix))
+		{//Something is wrong.
+			std::cout << " ### " << __FUNCTION__ << " ERROR: Unexpected line postfix, line: " << line << std::endl;
+			exit(-1);
+		}
+
+		// Delete starting whitespaces and trailing colon from the new group name.
+		currentGroupName = line
+						   .substr(2, std::string::npos)
+						   .substr(0, line.size() - (groupNameLinePrefix.size() + groupNameLinePostfix.size()));
+
+		lineGroups.push_back({currentGroupName, {}});
+		++currentPairIndex;
+	}
+
+	return lineGroups;
+}
+
 SlicerParams::SlicerParams(std::string cliParamsPath)
 {
 	std::ifstream fileStream(cliParamsPath);
@@ -18,14 +71,22 @@ SlicerParams::SlicerParams(std::string cliParamsPath)
 	const std::string fileContents((std::istreambuf_iterator<char>(fileStream)), std::istreambuf_iterator<char>());
 	fileStream.close();
 
+	// Read in parameter lines  - tokenize by newline delimiter.
 	std::regex delimiter("\n");
-	std::vector<std::string> tokens(std::sregex_token_iterator(fileContents.begin(), fileContents.end(), delimiter, -1),
-								   std::sregex_token_iterator());
+	const std::vector<std::string> lines(std::sregex_token_iterator(fileContents.begin(), fileContents.end(), delimiter, -1),
+										 std::sregex_token_iterator());
 
-	for (const auto& token : tokens)
+	// Extract parameter groups.
+	auto lineGroups = extractParameterGroups(lines);
+
+	for (const auto& [groupName, lineGroup]: lineGroups)
 	{
-		std::cout << " ### TOKEN:" << std::endl;
-		std::cout << token << std::endl;
+		std::cout << " ### " << __FUNCTION__ << " next group:" << groupName << std::endl;
+
+		for (const auto& line: lineGroup)
+		{
+			std::cout << line << std::endl;
+		}
 	}
 
 //	stream >> _params;
--- src/cpp/SlicerParams.h
+++ src/cpp/SlicerParams.h
@@ -13,5 +13,7 @@ public:
 	SlicerParams(std::string paramsPath);
 
 private:
+	std::vector<std::pair<std::string, std::vector<std::string>>> extractParameterGroups(const std::vector<std::string>& lines);
+
 	json _params;
 };
